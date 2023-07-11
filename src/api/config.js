db42f43f8510a1959ce5a53149236b4d51e7c019
Noah Templet <noahtemp13@gmail.com>
2023-07-10 20:44:22 -0500

    Update config again to use appropriate API url

--- src/api/config.js
+++ src/api/config.js
@@ -5,7 +5,7 @@ let API_HOST
 if(hostname === "dashboard.noahtemplet.dev")
   API_HOST = "https://api.noahtemplet.dev"
 else if (hostname === "qa.dashboard.noahtemplet.dev")
-  API_HOST = "https://qa.api.noahtemplet.dev"
+  API_HOST = "https://qa.api.noahtemplet.dev/api"
 else
   API_HOST = "http://localhost:5192/api"
 
