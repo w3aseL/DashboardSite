5dbc6ee3e079471e4b02bf87a84537d364eacf76
Noah Templet <noahtemp13@gmail.com>
2026-01-04 03:51:07 -0600

    Continued package updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { connect } from "react-redux"
 import { Container, Nav, Navbar, NavbarBrand, NavItem, NavLink, Dropdown, DropdownToggle, DropdownItem, DropdownMenu } from 'reactstrap';
-import { Avatar } from "@material-ui/core"
+import { Avatar } from "@mui/material"
 
 class HeaderComp extends React.Component {
   constructor(props) {
